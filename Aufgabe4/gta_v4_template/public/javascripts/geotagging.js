57fa48b104861151ea3ca2bfcfe466931f85dafb
Dominik1013 <ledo1013@h-ka.de>
2022-01-13 17:44:07 +0100

    Aufgabe4

---
+++ Aufgabe4/gta_v4_template/public/javascripts/geotagging.js
@@ -0,0 +1,174 @@
+// File origin: VS1LAB A2
+
+/* eslint-disable no-unused-vars */
+
+// This script is executed when the browser loads index.html.
+
+// "console.log" writes to the browser's console. 
+// The console window must be opened explicitly in the browser.
+// Try to find this output in the browser...
+console.log("The geoTagging script is going to start...");
+
+let clientTags = [];
+/**
+ * TODO: 'updateLocation'
+ * A function to retrieve the current location and update the page.
+ * It is called once the page has been fully loaded.
+ */
+ function updateLocation() {
+    let tagLat = document.getElementById("latitude_input")
+    let tagLong = document.getElementById("longitude_input")
+
+    console.log(tagLat);
+    console.log(tagLong);
+
+    let mapManager = new MapManager("EHhsx33AjvAofKoU9zPL366HAQnvBAwE");
+
+    let mapView = document.querySelector("#mapView");
+    let listElements = JSON.parse(mapView.getAttribute("data-tags"));
+
+    var mapURL = mapManager.getMapUrl(
+        document.getElementById("latitude_input").value, 
+        document.getElementById("longitude_input").value,
+        listElements
+    )
+    document.getElementById("mapView").setAttribute("src",mapURL) ;
+
+    // Erweitert um Abfrage
+    if (tagLat.value == null || tagLong.value == null ||
+        tagLat.value === "" || tagLong.value === "") {
+            console.log("it worked");
+        try {
+            LocationHelper.findLocation(updateDocument);
+        } catch (e) {
+            console.log("The GeoLocation API is currently unavailable.");
+        }
+    }
+}
+
+function updateDocument(helper) {
+    console.log("in function updateDocument");
+
+    document.getElementById("latitude_input").value = helper.latitude; 
+    document.getElementById("longitude_input").value = helper.longitude;
+    document.getElementById("discovery_latitude").value = helper.latitude;
+    document.getElementById("discovery_longitude").value = helper.longitude;
+
+    let mapManager = new MapManager("EHhsx33AjvAofKoU9zPL366HAQnvBAwE");
+    let tagList = document.getElementById("mapView").dataset.tags;
+
+    console.log("Taglist Log:" + tagList);
+
+    let mapURL = mapManager.getMapUrl(
+        helper.latitude,
+        helper.longitude,
+        JSON.parse(tagList)
+       /*
+        document.getElementById("latitude_input").value, 
+        document.getElementById("longitude_input").value,
+        */
+    );
+    document.getElementById("mapView").src = mapURL;
+}
+
+/* old location helper
+function updateLocation() {
+    try{
+        LocationHelper.findLocation(updateDocument);} 
+    catch(e){
+        alert("Fehler beim Auslesen der Position!");
+    }
+
+    let mapManager = new MapManager("EHhsx33AjvAofKoU9zPL366HAQnvBAwE");
+    var mapURL = mapManager.getMapUrl(
+        document.getElementById("latitude_input").value, 
+        document.getElementById("longitude_input").value,
+    )
+    document.getElementById("mapView").setAttribute("src",mapURL) ;
+}
+*/
+
+
+// Wait for the page to fully load its DOM content, then call updateLocation
+document.addEventListener("DOMContentLoaded", () => {
+    updateLocation();
+    getInitialValues();
+    document.getElementById("add_tag_button").addEventListener("click", function (){
+        let data = {
+            latitude: document.getElementById("latitude_input").value,
+            longitude: document.getElementById("longitude_input").value,
+            name: document.getElementById("name_input").value,
+            hashtag: document.getElementById("hashtag_input").value
+        }
+        if(!(data.hashtag.match(/#[a-zA-Z0-9]+/))){
+            return;
+        }
+        const xhttp = new XMLHttpRequest(),
+            method="POST",
+            url="http://localhost:3000/api/geotags";
+        xhttp.open(method, url, true);
+        xhttp.setRequestHeader("Content-Type","application/json;charset=UTF-8");
+
+
+        xhttp.onreadystatechange = function (){
+            console.log(xhttp.readyState);
+            if (xhttp.readyState === XMLHttpRequest.DONE && xhttp.status === 200)
+            {
+
+                console.log(xhttp.response);
+                const answer = JSON.parse(JSON.parse(xhttp.response));
+                //JSON.parse(JSON.parse(xhttp.response))
+                console.log(answer);
+                let element = document.createElement("li")
+                element.innerHTML = (`${answer.name} ${answer.latitude} ${answer.longitude} ${answer.hashtag}`);
+                document.getElementById("discoveryResults").appendChild(element);
+            }
+
+
+        }
+        xhttp.send(JSON.stringify(data));
+    })
+
+    document.getElementById("search_button").addEventListener("click", function () {
+
+        let searchWord = document.getElementById("search_input").value.toLowerCase();
+        let filterTags = clientTags.filter((tag) => {
+                return tag.name.toLowerCase().includes(searchWord) || tag.hashtag.toLowerCase().includes(searchWord);
+            }
+        );
+
+        document.getElementById("discoveryResults").innerHTML = "";
+        for (let i = 0; i < filterTags.length; i++) {
+            let element = document.createElement("li");
+            element.innerHTML = (`${filterTags[i].name} ${filterTags[i].latitude} ${filterTags[i].longitude} ${filterTags[i].hashtag}`);
+            document.getElementById("discoveryResults").appendChild(element);
+        }
+    });
+
+function getInitialValues(){
+    const xhttp = new XMLHttpRequest(),
+        method="GET",
+        url="http://localhost:3000/api/geotags";
+    xhttp.open(method, url, true);
+    xhttp.onreadystatechange = function (){
+        console.log(xhttp.readyState);
+        if (xhttp.readyState === XMLHttpRequest.DONE && xhttp.status === 200)
+        {
+            const answer = JSON.parse(xhttp.response);
+            console.log(answer);
+
+            for(let i = 0; i < answer.geotags.length; i++)
+            {
+                let element = document.createElement("li")
+                element.innerHTML = (`${answer.geotags[i].name} ${answer.geotags[i].latitude} ${answer.geotags[i].longitude} ${answer.geotags[i].hashtag}`);
+                document.getElementById("discoveryResults").appendChild(element);
+
+            }
+            clientTags = answer.geotags.slice();
+
+        }
+    }
+    xhttp.send();
+}})
+
+
--- Aufgabe4/gta_v4_template/routes/index.js
+++ Aufgabe4/gta_v4_template/routes/index.js
@@ -1,4 +1,4 @@
-// File origin: VS1LAB A3, A4
+// File origin: VS1LAB A3
 
 /**
  * This script defines the main router of the GeoTag server.
@@ -12,108 +12,107 @@
 
 const express = require('express');
 const router = express.Router();
+const tags = require('../models/geotag-examples');
+
+/*
+const app = express();
+app.use(express.json()); // for parsing application/json
+app.use(express.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
+*/
 
 /**
- * The module "geotag" exports a class GeoTagStore. 
+ * The module "geotag" exports a class GeoTagStore.
  * It represents geotags.
+ *
+ * DONE: implement the module in the file "../models/geotag.js"
  */
 // eslint-disable-next-line no-unused-vars
 const GeoTag = require('../models/geotag');
 
 /**
- * The module "geotag-store" exports a class GeoTagStore. 
+ * The module "geotag-store" exports a class GeoTagStore.
  * It provides an in-memory store for geotag objects.
+ *
+ * DONE: implement the module in the file "../models/geotag-store.js"
  */
 // eslint-disable-next-line no-unused-vars
 const GeoTagStore = require('../models/geotag-store');
 
-// App routes (A3)
+let memoryTags = tags.tagList;
 
-/**
- * Route '/' for HTTP 'GET' requests.
- * (http://expressjs.com/de/4x/api.html#app.get.method)
- *
- * Requests cary no parameters
- *
- * As response, the ejs-template is rendered without geotag objects.
- */
+router.get("/api/geotags", function (req, res){
+  res.json({geotags: memoryTags});
+})
+
+router.post("/api/geotags", function (req, res){
+  //let newTag = JSON.parse(req.body);
+  //memoryTags.push(GeoTag.constructor(req.body.name, req.body.latitude, req.body.longitude, req.body.hashtag));
+  memoryTags.push(new GeoTag(req.body.name, req.body.latitude, req.body.longitude, req.body.hashtag));
+  console.log(memoryTags);
+  res.json(JSON.stringify(req.body));
+})
 
 router.get('/', (req, res) => {
   res.render('index', { taglist: [] })
 });
 
-// API routes (A4)
-
 /**
- * Route '/api/geotags' for HTTP 'GET' requests.
- * (http://expressjs.com/de/4x/api.html#app.get.method)
- *
- * Requests contain the fields of the Discovery form as query.
- * (http://expressjs.com/de/4x/api.html#req.body)
- *
- * As a response, an array with Geo Tag objects is rendered as JSON.
- * If 'searchterm' is present, it will be filtered by search term.
- * If 'latitude' and 'longitude' are available, it will be further filtered based on radius.
- */
-
-// TODO: ... your code here ...
-
-
-/**
- * Route '/api/geotags' for HTTP 'POST' requests.
+ * Route '/tagging' for HTTP 'POST' requests.
  * (http://expressjs.com/de/4x/api.html#app.post.method)
  *
- * Requests contain a GeoTag as JSON in the body.
- * (http://expressjs.com/de/4x/api.html#req.query)
+ * Requests cary the fields of the tagging form in the body.
+ * (http://expressjs.com/de/4x/api.html#req.body)
  *
- * The URL of the new resource is returned in the header as a response.
- * The new resource is rendered as JSON in the response.
+ * Based on the form data, a new geotag is created and stored.
+ *
+ * As response, the ejs-template is rendered with geotag objects.
+ * All result objects are located in the proximity of the new geotag.
+ * To this end, "GeoTagStore" provides a method to search geotags
+ * by radius around a given location.
  */
+// DONE: ... your code here ...
+router.post('/tagging', (req, res) => {
+  //console.log(req.body);
+  let tag = new GeoTag(req.body["name_input"],
+      req.body["latitude_input"],
+      req.body["longitude_input"],
+      req.body["hashtag_input"]);
+  GeoTagStore.addGeoTag(tag);
+  res.render('index', {taglist: GeoTagStore.getNearbyGeoTags(tag, 100000)})
+});
 
-// TODO: ... your code here ...
-
+/*
+app.post('/tagging', function (req, res)  {
+  var gtag = new GeoTag(req.body.latitude,req.body.longitude,req.body.name,req.body.hashtag);
+  GeoTagStore.addGeoTag(gtag);
+  res.render('index', {taglist: GeoTagStore.getNearbyGeoTags(tag, 100000)});
+});
+*/
 
 /**
- * Route '/api/geotags/:id' for HTTP 'GET' requests.
- * (http://expressjs.com/de/4x/api.html#app.get.method)
+ * Route '/discovery' for HTTP 'POST' requests.
+ * (http://expressjs.com/de/4x/api.html#app.post.method)
  *
- * Requests contain the ID of a tag in the path.
- * (http://expressjs.com/de/4x/api.html#req.params)
+ * Requests cary the fields of the discovery form in the body.
+ * This includes coordinates and an optional search term.
+ * (http://expressjs.com/de/4x/api.html#req.body)
  *
- * The requested tag is rendered as JSON in the response.
+ * As response, the ejs-template is rendered with geotag objects.
+ * All result objects are located in the proximity of the given coordinates.
+ * If a search term is given, the results are further filtered to contain
+ * the term as a part of their names or hashtags.
+ * To this end, "GeoTagStore" provides methods to search geotags
+ * by radius and keyword.
  */
-
 // TODO: ... your code here ...
-
-
-/**
- * Route '/api/geotags/:id' for HTTP 'PUT' requests.
- * (http://expressjs.com/de/4x/api.html#app.put.method)
- *
- * Requests contain the ID of a tag in the path.
- * (http://expressjs.com/de/4x/api.html#req.params)
- * 
- * Requests contain a GeoTag as JSON in the body.
- * (http://expressjs.com/de/4x/api.html#req.query)
- *
- * Changes the tag with the corresponding ID to the sent value.
- * The updated resource is rendered as JSON in the response. 
- */
-
-// TODO: ... your code here ...
-
-
-/**
- * Route '/api/geotags/:id' for HTTP 'DELETE' requests.
- * (http://expressjs.com/de/4x/api.html#app.delete.method)
- *
- * Requests contain the ID of a tag in the path.
- * (http://expressjs.com/de/4x/api.html#req.params)
- *
- * Deletes the tag with the corresponding ID.
- * The deleted resource is rendered as JSON in the response.
- */
-
-// TODO: ... your code here ...
+router.post('/discovery', (req, res) => {
+  console.log(req.body);
+  console.log("req.body[search_input] " + req.body["search_input"]);
+  let queryTag = new GeoTag('Query',
+      req.body["discovery_latitude"],
+      req.body["discovery_longitude"],
+      '#query');
+  res.render('index', {taglist: GeoTagStore.searchNearbyGeoTags(queryTag, 100000, req.body["search_input"]) })
+});
 
 module.exports = router;
